4a81aab5a3e8378ff5157124f81137405e7408ef
Anthony Burroughs <bonfilio.tony@gmail.com>
2020-04-29 14:09:06 -0500

    Heroku deployment troubleshooting

--- server/index.js
+++ server/index.js
@@ -32,10 +32,12 @@ require('./routes/billingRoutes')(app);
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // Like main.js and main.css
-  app.use(express.static('client/build'));
+  const path = require('path');
+  //app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'client/build')));
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require('path');
+
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
